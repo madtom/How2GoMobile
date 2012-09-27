bb9679b98d7d6d674e72dc1a536d5129c7476c6b
CrazyNexus <info@crazynexus.de>
2012-09-28 00:06:38 +0200

    finished UI design

---
+++ How2Go/How2Go/CNX_ExpensesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CNX_ExpensesTableViewController.h
+//  How2Go
+//
+//  Created by Thomas Dubiel on 27.09.12.
+//  Copyright (c) 2012 Thomas Dubiel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CNX_ExpensesTableViewController : UITableViewController
+
+@end
